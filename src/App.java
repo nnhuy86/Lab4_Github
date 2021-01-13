71d60a8cd1289f0014d81cc0d6a1be46c025f2b9
nnhuy86 <nguyenhuy.86nt@gmail.com>
2021-01-13 10:51:25 +0700

    Huy push file

---
+++ src/App.java
@@ -0,0 +1,22 @@
+import java.awt.EventQueue;
+
+import controller.LoginController;
+import view.LoginView;
+
+public class App {
+
+	public static void main(String[] args)
+	{
+		EventQueue.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				LoginView view = new LoginView();
+				LoginController controller = new LoginController(view);
+				//hien thi man hinh login
+				controller.showLoginView();
+			}
+		});
+	}
+}
+
